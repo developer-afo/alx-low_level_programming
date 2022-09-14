44deee4b4f08409eb7b52ba4e14ea749389e8a84
Afolabi Salawu <salawuhamid96@gmail.com>
2022-09-14 01:18:59 +0100

    my commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,33 +1,45 @@
 #include <stdio.h>
-
 /**
- * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
- *
- * Return: Always 0.
- */
-
+*main - prints out first 98
+*fibonacci suit numbers
+*Return: return 0
+*/
 int main(void)
 {
-	long int sum;
-	long int first, second;
-	int count;
+int inc;
+unsigned long n1 = 0, n2 = 1, n3;
+unsigned long n1_h1, n1_h2, n2_h1, n2_h2;
+unsigned long h1, h2;
 
-	sum = 0;
-	first = 0;
-	second = 1;
-	count = 0;
+for (inc = 0; inc < 92; inc++)
+{
+n3 = n1 + n2;
+printf("%lu, ", n3);
+n1 = n2;
+n2 = n3;
+}
+n1_h1 = n1 / 10000000000;
+n2_h1 = n2 / 10000000000;
+n1_h2 = n1 % 10000000000;
+n2_h2 = n2 % 10000000000;
+for (inc = 93; inc < 99; inc++)
+{
+h1 = n1_h1 + n2_h1;
+h2 = n1_h2 + n2_h2;
+if ((n1_h2 + n2_h2) > 9999999999)
+{
+h1 += 1;
+h2 %= 10000000000;
+}
+printf("%lu%lu", h1, h2);
+if (inc != 98)
+printf(", ");
 
-	while (count < 98)
-	{
-		sum = first + second;
-		printf("%li, ", sum);
-		first = second;
-		second = sum;
-
-		count++;
-	}
-	sum = first + second;
-	printf("%li\n", sum);
-
-	return (0);
+n1_h1 = n2_h1;
+n1_h2 = n2_h2;
+n2_h1 = h1;
+n2_h2 = h2;
+}
+printf("\n");
+return (0);
 }
